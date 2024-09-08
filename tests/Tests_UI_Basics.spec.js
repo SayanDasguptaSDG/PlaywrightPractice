7bc1e2e345b71f2ed890de4a848b3813ffaf0806
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-08 23:45:28 +0530

    playwright tests

---
+++ tests/Tests_UI_Basics.spec.js
@@ -0,0 +1,11 @@
+const {test} = require('@playwright/test');
+
+test('BrowserContext Playwright test', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test('Page Playwright test', async ({page}) => {
+    await page.goto("https://google.com");
+});
