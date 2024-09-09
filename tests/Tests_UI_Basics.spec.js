3d0afd33bda6b97607ca084c29dbb2dd8ed3496b
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-09 10:52:47 +0530

    UI Validations

--- tests/Tests_UI_Basics.spec.js
+++ tests/Tests_UI_Basics.spec.js
@@ -11,3 +11,12 @@ test('Page Playwright test', async ({page}) => {
     console.log(await page.title());
     await expect(page).toHaveTitle("Google");
 });
+
+test.only('Web Browser Context Validation Error Login', async ({page}) => {
+    await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator("input[id='username']").fill("rahulshetty");
+    await page.locator("input[id='password']").fill("learning");
+    await page.locator("#signInBtn").click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+});
