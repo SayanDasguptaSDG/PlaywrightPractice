ac2e09c0329df56c34bf159434f02d380bbd470f
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-21 20:31:11 +0530

    to find element or elements returned by the locator

--- tests/Tests_UI_Basics.spec.js
+++ tests/Tests_UI_Basics.spec.js
@@ -12,11 +12,21 @@ test('Page Playwright test', async ({page}) => {
     await expect(page).toHaveTitle("Google");
 });
 
-test.only('Web Browser Context Validation Error Login', async ({page}) => {
+test('Web Browser Context Validation Error Login', async ({page}) => {
     await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
     await page.locator("input[id='username']").fill("rahulshetty");
     await page.locator("input[id='password']").fill("learning");
     await page.locator("#signInBtn").click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+});
+
+test.only('Web Browser Context Validation', async ({page}) => {
+    await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator("input[id='username']").fill("rahulshettyacademy");
+    await page.locator("input[id='password']").fill("learning");
+    await page.locator("#signInBtn").click();
+    console.log(await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+    console.log(await page.locator(".card-body a").allTextContents());
 });
