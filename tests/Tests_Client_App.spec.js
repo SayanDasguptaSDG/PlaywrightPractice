434f961e41bbc7aae303f4f015ecb15a377e492b
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-22 10:47:45 +0530

    wait mechanism in playwright

---
+++ tests/Tests_Client_App.spec.js
@@ -0,0 +1,12 @@
+const {test} = require('@playwright/test');
+
+test.only('Element Validation', async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/client/");
+    await page.locator("input[id='userEmail']").fill("training.shreesha@gmail.com");
+    await page.locator("input[id='userPassword']").fill("Train123");
+    await page.locator("#login").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card b").last().waitFor();
+    const titles = await page.locator(".card b").allTextContents();
+    console.log(titles);
+});
--- tests/Tests_UI_Basics.spec.js
+++ tests/Tests_UI_Basics.spec.js
@@ -21,7 +21,7 @@ test('Web Browser Context Validation Error Login', async ({page}) => {
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
 });
 
-test.only('Web Browser Context Validation', async ({page}) => {
+test('Web Browser Context Validation', async ({page}) => {
     await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
     await page.locator("input[id='username']").fill("rahulshettyacademy");
     await page.locator("input[id='password']").fill("learning");
