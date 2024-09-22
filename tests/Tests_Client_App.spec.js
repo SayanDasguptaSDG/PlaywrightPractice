3e80191e3439ce6d777559b7f55a6e22a6f984bf
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-09-22 12:32:58 +0530

    to handle select dropdown, radio buttons, checkboxes and blinking texts, and assertion around them based on checkability and having attribute value

--- tests/Tests_Client_App.spec.js
+++ tests/Tests_Client_App.spec.js
@@ -1,6 +1,6 @@
 const {test} = require('@playwright/test');
 
-test.only('Element Validation', async ({page}) => {
+test('Element Validation', async ({page}) => {
     await page.goto("https://rahulshettyacademy.com/client/");
     await page.locator("input[id='userEmail']").fill("training.shreesha@gmail.com");
     await page.locator("input[id='userPassword']").fill("Train123");
--- tests/Tests_UI_Basics.spec.js
+++ tests/Tests_UI_Basics.spec.js
@@ -30,3 +30,26 @@ test('Web Browser Context Validation', async ({page}) => {
     console.log(await page.locator(".card-body a").nth(1).textContent());
     console.log(await page.locator(".card-body a").allTextContents());
 });
+
+test.only('UI Controls', async ({page}) => {
+    await page.goto("http://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator("input[id='username']").fill("rahulshettyacademy");
+    await page.locator("input[id='password']").fill("learning");
+
+    await page.locator(".customradio [value='user']").click();
+    await page.locator("#okayBtn").click();
+    await expect(page.locator(".customradio [value='user']")).toBeChecked();
+
+    const dropdown = page.locator("select.form-control")
+    await dropdown.selectOption("consult");
+
+    await page.locator("input[type='checkbox']").click();
+    await expect(page.locator("input[type='checkbox']")).toBeChecked();
+    await page.locator("input[type='checkbox']").uncheck();
+    expect(await page.locator("input[type='checkbox']").isChecked()).toBeFalsy();
+
+    const documentsLink = page.locator("a[href*='documents-request']");
+    await expect(documentsLink).toHaveAttribute("class", "blinkingText");
+
+    await page.locator("#signInBtn").click();
+});
